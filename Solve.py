eac264e03f0ddadf22e6fe17cc242bc3b8408582
shooter <shooter.il@yandex.ua>
2015-10-27 01:56:27 +0200

    add Solve.py

---
+++ Solve.py
@@ -0,0 +1,9 @@
+__author__ = 'strike'
+
+class Solve(object):
+    '''
+    {'samples': 50, 'input_file': '', 'dimentions': array([3, 1, 2, 2]), 'output_file': '', 'degrees': array([3, 3, 3]), 'lambda_multiblock': False, 'weights': 'average', 'poly_type': 'chebyshev'}
+[26.10.2015 0:21:17] ‘¿“: {'lambda_multiblock': False, 'degrees': array([3, 3, 3]), 'samples': 50, 'input_file': '/Users/vlad/PycharmProjects/sys_an_labs/data.txt', 'output_file': '/Users/vlad/PycharmProjects/sys_an_labs/data1.txt', 'dimentions': array([3, 1, 2, 2]), 'poly_type': 'chebyshev', 'weights': 'average'}
+[26.10.2015 0:21:52] ‘¿“: {'lambda_multiblock': True, 'degrees': array([3, 3, 3]), 'samples': 50, 'input_file': '/Users/vlad/PycharmProjects/sys_an_labs/data.txt', 'output_file': '/Users/vlad/PycharmProjects/sys_an_labs/data1.txt', 'dimentions': array([3, 1, 2, 2]), 'poly_type': 'lagger', 'weights': 'scaled'}
+    '''
+    def __init__(self,n, ):
