a38fb4785f137760e96b8d1b4515f3b53bb19c7c
Megalitum <fatvlad.95@gmail.com>
2015-11-02 16:43:57 +0200

    little refactor

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ __author__ = 'vlad'
 if __name__ == '__main__':
     number = 2
     if not number:
-        global number
         number = input('Which lab to launch => ')
     filename = 'lab_{0}/main.py'.format(number)
     with open(filename,'r') as f:
