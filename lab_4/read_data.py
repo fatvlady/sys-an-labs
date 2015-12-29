84d85d35eb78c46a490954e5969bf280f4908de8
Megalitum <fatvlad.95@gmail.com>
2015-12-29 10:14:29 +0200

    Fixed data scan.

--- lab_4/read_data.py
+++ lab_4/read_data.py
@@ -5,5 +5,6 @@ def read_data(filename = 'norm.xlsx'):
     xl_file = pd.ExcelFile(filename)
     # print(xl_file.sheet_names[0])
     dfs = xl_file.parse(xl_file.sheet_names[0])
+    dfd = dfs.as_matrix()
     t = dfs.T.columns.values.tolist()
-    return t, dfs
+    return t, dfd
